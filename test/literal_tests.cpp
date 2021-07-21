b90f86d304b88a8007147aba238430472b33d0d4
Louis Christner <louis.christner@epitech.eu>
2021-07-21 18:52:09 +0200

    Add exception throwing tests

--- test/literal_tests.cpp
+++ test/literal_tests.cpp
@@ -14,6 +14,16 @@ Test(literal_tests, ensure_that_addition_occurs_without_technical_errors)
     cr_assert(engine.stack.pop().getValue() == "2");
 }
 
+Test(literal_tests, ensure_that_invalid_arguments_addition_produces_valid_error)
+{
+    Engine engine;
+
+    engine.stack.add(Literal(Literal::STRING, "one"));
+    engine.stack.add(Literal(Literal::BOOLEAN, "1"));
+
+    cr_assert_throw(engine.operations.get("+")(engine), InvalidTypeException);
+}
+
 Test(literal_tests, ensure_that_substraction_occurs_without_technical_errors)
 {
     Engine engine;
@@ -24,6 +34,16 @@ Test(literal_tests, ensure_that_substraction_occurs_without_technical_errors)
     engine.operations.get("-")(engine);
 
     cr_assert(engine.stack.pop().getValue() == "0");
+}
+
+Test(literal_tests, ensure_that_invalid_arguments_substraction_produces_valid_error)
+{
+    Engine engine;
+
+    engine.stack.add(Literal(Literal::STRING, "one"));
+    engine.stack.add(Literal(Literal::BOOLEAN, "1"));
+
+    cr_assert_throw(engine.operations.get("-")(engine), InvalidTypeException);
 }
 
 Test(literal_tests, ensure_that_multiplication_occurs_without_technical_errors)
@@ -38,6 +58,16 @@ Test(literal_tests, ensure_that_multiplication_occurs_without_technical_errors)
     cr_assert(engine.stack.pop().getValue() == "2");
 }
 
+Test(literal_tests, ensure_that_invalid_arguments_multiplication_produces_valid_error)
+{
+    Engine engine;
+
+    engine.stack.add(Literal(Literal::STRING, "two"));
+    engine.stack.add(Literal(Literal::BOOLEAN, "1"));
+
+    cr_assert_throw(engine.operations.get("*")(engine), InvalidTypeException);
+}
+
 Test(literal_tests, ensure_that_division_occurs_without_technical_errors)
 {
     Engine engine;
@@ -50,6 +80,16 @@ Test(literal_tests, ensure_that_division_occurs_without_technical_errors)
     cr_assert(engine.stack.pop().getValue() == "1");
 }
 
+Test(literal_tests, ensure_that_invalid_arguments_division_produces_valid_error)
+{
+    Engine engine;
+
+    engine.stack.add(Literal(Literal::STRING, "one"));
+    engine.stack.add(Literal(Literal::BOOLEAN, "1"));
+
+    cr_assert_throw(engine.operations.get("/")(engine), InvalidTypeException);
+}
+
 Test(literal_tests, ensure_that_modulo_occurs_without_technical_errors)
 {
     Engine engine;
@@ -60,6 +100,16 @@ Test(literal_tests, ensure_that_modulo_occurs_without_technical_errors)
     engine.operations.get("%")(engine);
 
     cr_assert(engine.stack.pop().getValue() == "0");
+}
+
+Test(literal_tests, ensure_that_invalid_arguments_modulo_produces_valid_error)
+{
+    Engine engine;
+
+    engine.stack.add(Literal(Literal::STRING, "4"));
+    engine.stack.add(Literal(Literal::BOOLEAN, "2"));
+
+    cr_assert_throw(engine.operations.get("+")(engine), InvalidTypeException);
 }
 
 Test(literal_tests, ensure_that_true_integer_comparison_occurs_without_technical_errors)
@@ -170,6 +220,16 @@ Test(literal_tests, ensure_that_false_greater_than_comparison_between_integer_oc
     cr_assert(engine.stack.pop().getValue() == "false");
 }
 
+Test(literal_tests, ensure_that_invalid_arguments_greater_than_comparison_produces_valid_error)
+{
+    Engine engine;
+
+    engine.stack.add(Literal(Literal::STRING, "two"));
+    engine.stack.add(Literal(Literal::BOOLEAN, "3"));
+
+    cr_assert_throw(engine.operations.get(">")(engine), InvalidTypeException);
+}
+
 Test(literal_tests, ensure_that_true_lower_than_comparison_between_integer_occurs_without_technical_errors)
 {
     Engine engine;
@@ -177,7 +237,7 @@ Test(literal_tests, ensure_that_true_lower_than_comparison_between_integer_occur
     engine.stack.add(Literal(Literal::INTEGER, "2"));
     engine.stack.add(Literal(Literal::INTEGER, "3"));
 
-    engine.operations.get(">")(engine);
+    engine.operations.get("<")(engine);
 
     cr_assert(engine.stack.pop().getValue() == "true");
 }
@@ -189,9 +249,19 @@ Test(literal_tests, ensure_that_false_lower_than_comparison_between_integer_occu
     engine.stack.add(Literal(Literal::INTEGER, "3"));
     engine.stack.add(Literal(Literal::INTEGER, "2"));
 
-    engine.operations.get(">")(engine);
+    engine.operations.get("<")(engine);
 
     cr_assert(engine.stack.pop().getValue() == "false");
+}
+
+Test(literal_tests, ensure_that_invalid_arguments_lower_than_comparison_produces_valid_error)
+{
+    Engine engine;
+
+    engine.stack.add(Literal(Literal::STRING, "two"));
+    engine.stack.add(Literal(Literal::BOOLEAN, "3"));
+
+    cr_assert_throw(engine.operations.get("<")(engine), InvalidTypeException);
 }
 
 Test(literal_tests, ensure_that_true_greater_or_equal_comparison_between_integer_occurs_without_technical_errors)
@@ -230,6 +300,16 @@ Test(literal_tests, ensure_that_false_greater_or_equal_comparison_between_intege
     cr_assert(engine.stack.pop().getValue() == "false");
 }
 
+Test(literal_tests, ensure_that_invalid_arguments_greater_or_equal_comparison_produces_valid_error)
+{
+    Engine engine;
+
+    engine.stack.add(Literal(Literal::STRING, "two"));
+    engine.stack.add(Literal(Literal::BOOLEAN, "3"));
+
+    cr_assert_throw(engine.operations.get(">=")(engine), InvalidTypeException);
+}
+
 Test(literal_tests, ensure_that_true_lower_or_equal_comparison_between_integer_occurs_without_technical_errors)
 {
     Engine engine;
@@ -264,4 +344,14 @@ Test(literal_tests, ensure_that_false_lower_or_equal_comparison_between_integer_
     engine.operations.get("<=")(engine);
 
     cr_assert(engine.stack.pop().getValue() == "false");
+}
+
+Test(literal_tests, ensure_that_invalid_arguments_lower_or_equal_comparison_produces_valid_error)
+{
+    Engine engine;
+
+    engine.stack.add(Literal(Literal::STRING, "two"));
+    engine.stack.add(Literal(Literal::BOOLEAN, "3"));
+
+    cr_assert_throw(engine.operations.get("<=")(engine), InvalidTypeException);
 }
