8fe43d77824d487158e436e40fc4bcf89e0c8f54
Louis Christner <louis.christner@epitech.eu>
2022-04-14 11:02:38 +0200

    Add reader tests

---
+++ test/reader_tests.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <criterion/criterion.h>
+
+#include "../src/reader/reader.hpp"
+
+Test(reader_tests, ensure_that_reader_occurs_without_technical_errors)
+{
+    cr_assert(reader("../examples/example0.txt") == "1 1 +");
+}
+
+Test(reader_tests, ensure_that_invalid_file_reader_throw_exception)
+{
+    cr_assert_throw(reader("../examples/example0.txt"), InvalidReaderException);
+}
