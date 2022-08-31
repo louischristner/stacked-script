db20345233410c9b19ea632cfb2d0c54650ecd8e
Louis Christner <louis.christner@epitech.eu>
2022-08-31 21:21:27 +0200

    Fix test issues

--- test/reader_tests.cpp
+++ test/reader_tests.cpp
@@ -10,5 +10,5 @@ Test(reader_tests, ensure_that_reader_occurs_without_technical_errors)
 
 Test(reader_tests, ensure_that_invalid_file_reader_throw_exception)
 {
-    cr_assert_throw(reader("../examples/example0.txt"), InvalidReaderException);
+    cr_assert_throw(reader("../strange.txt"), InvalidReaderException);
 }
